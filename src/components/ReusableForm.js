7edf1c9fa63de3aff01e3909cf527511b7c73294
Ian Gregg <iangregg188@gmail.com>
2020-09-22 09:21:27 -0700

    added in the reuseable form.
    Co-authored-by: RichterBros <erich.richter@gmail.com>
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <Form onSubmit={props.formSubmissionHandler}>
+        <Form.Group>
+          <Form.Control
+            type='text'
+            name='names'
+            placeholder='Names' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type='text'
+            name='question1'
+            placeholder='Question1.' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            as='textarea'
+            name='question2'
+            placeholder='Question2.' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            as='textarea'
+            name='question3'
+            placeholder='Question3.' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            as='textarea'
+            name='question4'
+            placeholder='Question4.' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            as='textarea'
+            name='question5'
+            placeholder='Question5.' />
+        </Form.Group>
+        <div style={{padding: 10}}>
+          <Button variant="info" type='submit'>{props.buttonText}</Button>
+        </div>
+      </Form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
